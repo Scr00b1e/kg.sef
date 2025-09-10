f0569d4578ef7873a9c0eac5e84003a7b5185317
Scr00b1e <kbolot312@gmail.com>
2025-09-10 22:45:06 +0600

    added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Home from './pages/Home'
 
 function App() {
 
@@ -8,7 +9,9 @@ function App() {
     <>
       <div className='App'>
         <Header/>
-        Hello world
+        <div className="container">
+          <Home/>
+        </div>
       </div>
     </>
   )
