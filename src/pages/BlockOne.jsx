466c96ae88f1e87e4fc8f9a6018795bf1c440b3f
Jack <kbolot312@gmail.com>
2025-10-12 02:37:13 +0600

    changed blockOne

--- src/pages/BlockOne.jsx
+++ src/pages/BlockOne.jsx
@@ -7,11 +7,24 @@ const BlockOne = () => {
             <div className='blockOne'>
                 <div className='blockOne_content'>
                     <div className="blockOne_words">
-                        <h1 className='blockOne_title'>Вдохновляем новое поколение ученых Кыргызстана</h1>
-                        <p className='blockOne_text'>KG.SEF — национальная научно-инженерная ярмарка, где старшеклассники представляют инновационные проекты, развивают научные навыки и готовятся к международным соревнованиям, таким как ISEF.</p>
+                        <div className="blockOne_info">
+                            <h1 className='blockOne_title'>Вдохновляем новое поколение ученых Кыргызстана</h1>
+                            <p className='blockOne_text'>KG.SEF — национальная научно-инженерная ярмарка, где старшеклассники представляют инновационные проекты, развивают научные навыки и готовятся к международным соревнованиям, таким как ISEF.</p>
+                        </div>
+                        <div className="blockOne_numbers">
+                            <div className="blockOne_numberOne">
+                                <h1>1800</h1>
+                                <p>Ярмарок по всему миру</p>
+                            </div>
+                            <div className="blockOne_numberOne">
+                                <h1>175 000</h1>
+                                <p>Участников участвующих в ярмарке</p>
+                            </div>
+                        </div>
+                        <button className='blockOne_button'>Учавствовать</button>
                     </div>
                     <div className='blockOne_img'>
-                        block2
+                        <img src="src/assets/students.png" alt="" />
                     </div>
                 </div>
             </div>
