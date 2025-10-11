b8bd50f2eea1c1421dbe9bac6c0484850568951d
Jack <kbolot312@gmail.com>
2025-10-12 01:54:26 +0600

    style changes

--- src/pages/BlockOne.jsx
+++ src/pages/BlockOne.jsx
@@ -7,10 +7,12 @@ const BlockOne = () => {
             <div className='blockOne'>
                 <div className='blockOne_content'>
                     <div className="blockOne_words">
-                        <h1 className='blockOne_title'>Кыргызстанская научно-инженерная ярмарка</h1>
-                        <p className='blockOne_text'>Ярмарка для выдающихся личностей которые хотят увидеть что то новое</p>
+                        <h1 className='blockOne_title'>Вдохновляем новое поколение ученых Кыргызстана</h1>
+                        <p className='blockOne_text'>KG.SEF — национальная научно-инженерная ярмарка, где старшеклассники представляют инновационные проекты, развивают научные навыки и готовятся к международным соревнованиям, таким как ISEF.</p>
                     </div>
-                    <button className='blockOne_btn'>Участвовать</button>
+                    <div className='blockOne_img'>
+                        block2
+                    </div>
                 </div>
             </div>
         </div>
